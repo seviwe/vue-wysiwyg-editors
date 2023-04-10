9d56e834246bd1ad6ce26200a482db5d7ad77642
Vsevolod <sevaaleynikov@gmail.com>
2023-04-10 15:37:20 +0300

    init

---
+++ src/components/WYSIWYG-editors.vue
@@ -0,0 +1,91 @@
+<template>
+	<div>
+		<div class="container">
+			<div class="container__row">
+				<div class="container__editor">
+					<h2>
+						Quill <span style="color: green">(MIT License)</span>&nbsp;
+						<a target="_blank" href="https://github.com/davidroyer/vue2-editor"
+							>Ссылка</a
+						>
+					</h2>
+					<QuillEdtior placeholder="Введите текст..." style="height: 450px"></QuillEdtior>
+				</div>
+				<div class="container__editor">
+					<h2>
+						Tiny
+						<a
+							target="_blank"
+							href="https://www.tiny.cloud/docs/tinymce/6/basic-example/"
+							>Ссылка</a
+						>
+					</h2>
+					<Editor
+						api-key="544lxuxspfxlhi9osq0dvb5wjp73ixajwkb1lucuyavg5knp"
+						:init="{
+							plugins: 'lists link image table help wordcount',
+							height: 498,
+							placeholder: 'Введите текст...',
+						}"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<h2>
+				TOAST UI Editor
+				<a
+					target="_blank"
+					href="https://github.com/nhn/tui.editor/tree/master/apps/vue-editor"
+					>Ссылка</a
+				>
+			</h2>
+			<ToastUIEditor height="500px" />
+		</div>
+		<div class="container">
+			<h2>
+				mavonEditor <span style="color: green">(MIT License)</span>&nbsp;
+				<a target="_blank" href="http://www.mavoneditor.com/">Ссылка</a>
+			</h2>
+			<mavon-editor language="ru" style="height: 500px" />
+		</div>
+	</div>
+</template>
+
+<script>
+import { VueEditor as QuillEdtior } from 'vue2-editor';
+
+import Editor from '@tinymce/tinymce-vue';
+
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor as ToastUIEditor } from '@toast-ui/vue-editor';
+
+export default {
+	name: 'WYSIWEGEditors',
+	props: {
+		msg: String,
+	},
+	components: {
+		QuillEdtior,
+		Editor,
+		ToastUIEditor,
+	},
+};
+</script>
+
+<style scoped>
+.container {
+	display: flex;
+	gap: 10px;
+	flex-direction: column;
+}
+
+.container__row {
+	display: flex;
+	gap: 10px;
+}
+
+.container__editor {
+	width: 50%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import Vue from 'vue'; import mavonEditor from 'mavon-editor'; import
+'mavon-editor/dist/css/index.css'; import App from './App.vue'; Vue.config.productionTip = false;
+Vue.use(mavonEditor); new Vue({ render: h => h(App), }).$mount('#app');
